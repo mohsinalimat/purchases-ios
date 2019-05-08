5cf0c3f1597939d81d5c6eafbe1a27e80ea56349
Alfonso Perez <alfon@ratslap.com>
2019-05-08 15:34:06 -0700

    WIP add lifetime button

--- Examples/SwiftExample/SwiftExample/UpsellViewController.swift
+++ Examples/SwiftExample/SwiftExample/UpsellViewController.swift
@@ -13,12 +13,18 @@ class UpsellViewController: UIViewController {
     
     @IBOutlet weak var annualCatsButton: UIButton!
     @IBOutlet weak var monthlyCatsButton: UIButton!
+    @IBOutlet weak var lifetimeCatsButton: UIButton!
+
     @IBOutlet weak var annualLoadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var monthlyLoadingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var lifetimeLoadingIndicator: UIActivityIndicatorView!
     
     var annualCatProduct : SKProduct?
     var monthlyCatProduct : SKProduct?
+    var lifetimeCatProduct : SKProduct?
+    
     var annualCatButtonTitle = ""
+    var lifetimeCatButtonTitle = ""
     var monthlyCatButtonTitle = ""
     
     override func viewDidLoad() {
@@ -26,6 +32,8 @@ class UpsellViewController: UIViewController {
         
         annualCatsButton.addTarget(self, action: #selector(buyAnnualCats), for: .touchDown)
         monthlyCatsButton.addTarget(self, action: #selector(buyMonthlyCats), for: .touchDown)
+        lifetimeCatsButton.addTarget(self, action: #selector(buyLifetimeCats), for: .touchDown)
+        
         setState(loading: true)
 
         Purchases.shared.entitlements { (entitlements, error) in
@@ -45,6 +53,10 @@ class UpsellViewController: UIViewController {
                 print("Error finding annual_cats offering")
                 return
             }
+            guard let lifetime = pro.offerings["lifetime_cats"] else {
+                print("Error finding annual_cats offering")
+                return
+            }
             
             guard let monthlyProduct = monthly.activeProduct else {
                 print("Error finding monthly active product")
@@ -54,15 +66,21 @@ class UpsellViewController: UIViewController {
                 print("Error finding annual active product")
                 return
             }
+            guard let lifetimeProduct = lifetime.activeProduct else {
+                print("Error finding lifetime active product")
+                return
+            }
             
             self.monthlyCatProduct = monthlyProduct
             self.annualCatProduct = annualProduct
+            self.lifetimeCatProduct = lifetimeProduct
             
             print("All entitlements fetched successfully 🎉")
             
             // set up the buttons
             self.annualCatButtonTitle = "Buy Annual - \(annualProduct.priceLocale.currencySymbol ?? "")\(annualProduct.price)"
             self.monthlyCatButtonTitle = "Buy Monthly - \(monthlyProduct.priceLocale.currencySymbol ?? "")\(monthlyProduct.price)"
+            self.lifetimeCatButtonTitle = "Buy Lifetime - \(lifetimeProduct.priceLocale.currencySymbol ?? "")\(lifetimeProduct.price)"
             self.setState(loading: false)
         }
     }
@@ -74,6 +92,11 @@ class UpsellViewController: UIViewController {
     
     @objc func buyMonthlyCats() {
         guard let product = monthlyCatProduct else { return }
+        makePurchase(catProduct: product)
+    }
+    
+    @objc func buyLifetimeCats() {
+        guard let product = lifetimeCatProduct else { return }
         makePurchase(catProduct: product)
     }
     
@@ -109,21 +132,27 @@ class UpsellViewController: UIViewController {
         if loading {
             annualLoadingIndicator.startAnimating()
             monthlyLoadingIndicator.startAnimating()
+            lifetimeLoadingIndicator.startAnimating()
             
             annualCatsButton.isEnabled = false
             monthlyCatsButton.isEnabled = false
+            lifetimeCatsButton.isEnabled = false
             
             annualCatsButton.setTitle(nil, for: .normal)
             monthlyCatsButton.setTitle(nil, for: .normal)
+            lifetimeCatsButton.setTitle(nil, for: .normal)
         } else {
             annualLoadingIndicator.stopAnimating()
             monthlyLoadingIndicator.stopAnimating()
+            lifetimeLoadingIndicator.stopAnimating()
             
             annualCatsButton.isEnabled = true
             monthlyCatsButton.isEnabled = true
+            lifetimeCatsButton.isEnabled = true
             
             annualCatsButton.setTitle(annualCatButtonTitle, for: .normal)
             monthlyCatsButton.setTitle(monthlyCatButtonTitle, for: .normal)
+            lifetimeCatsButton.setTitle(lifetimeCatButtonTitle, for: .normal)
         }
     }
 }
